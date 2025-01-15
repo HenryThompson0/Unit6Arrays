df7b94538ca1266352446632e556d37cfd7c673f
HenryThompson0 <sayjokesphotos@gmail.com>
2025-01-15 10:43:21 -0500

    Update RayOddToEven.java

--- RayOddToEven.java
+++ RayOddToEven.java
@@ -7,6 +7,34 @@ public class RayOddToEven
 {
 	public static int go(int[] ray)
 	{
-		return 0;
+		int i = 0;
+		int x = 0;
+		int nextEven=0;
+		int firstOdd=0;
+		
+		while (i<ray.length)
+		{
+			if (x==0)
+			{
+				if ((ray[i])%2!=0)
+				{
+					firstOdd = i;
+					x=1;
+				}
+			}
+
+			if (x==1)
+			{
+				if ((ray[i])%2==0)
+				{
+					nextEven = i;
+					x=2;
+				}
+			}
+
+			i+=1;	
+		}
+
+		return nextEven-firstOdd;
 	}
 }
