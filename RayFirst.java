cd321a786f9d0526c9ed02a56e065b01a44e9bae
HenryThompson0 <sayjokesphotos@gmail.com>
2025-01-22 09:42:21 -0500

    Update RayFirst.java

--- RayFirst.java
+++ RayFirst.java
@@ -7,6 +7,20 @@ public class RayFirst
 {
 	public static boolean go(int[] ray)
 	{
-		return true;
+        boolean isThere = false;
+        int i = 1;
+        int firstNum = ray[0];
+
+        while(isThere == false && i<ray.length)
+        {
+            if (ray[i]==firstNum)
+            {
+                isThere = true;
+                return true;
+            }
+            i++;
+        }
+
+        return false;
 	}
 }
