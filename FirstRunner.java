5c8d41dff4214d7cd9ad47c001c794357a314d7e
HenryThompson0 <sayjokesphotos@gmail.com>
2025-01-22 09:42:08 -0500

    Update FirstRunner.java

--- FirstRunner.java
+++ FirstRunner.java
@@ -7,5 +7,7 @@ public class FirstRunner
 {
 	public static void main( String args[] )
 	{
+        int[] ray = {-99,1,2,3,4,5,6,7,8,9,10,12345};
+        System.out.println(RayFirst.go(ray));
 	}
 }
