ae386b251d07f33da93e4f02f6f7edb593ab6182
HenryThompson0 <sayjokesphotos@gmail.com>
2025-01-27 09:32:30 -0500

    Update SmallestRunner.java

--- SmallestRunner.java
+++ SmallestRunner.java
@@ -6,6 +6,9 @@
 public class SmallestRunner
 {
 	public static void main( String args[] )
-	{									
+	{			
+        int[] Ray = {10,20,30,40,50,-11818,40,30,20,10};
+
+        System.out.println(RaySmallest.go(Ray));						
 	}
 }
