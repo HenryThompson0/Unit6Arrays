7a88860091fa83d9d5ab9df4dc78d5a7749e6108
HenryThompson0 <sayjokesphotos@gmail.com>
2025-01-27 09:32:17 -0500

    Update RaySmallest.java

--- RaySmallest.java
+++ RaySmallest.java
@@ -7,6 +7,18 @@ public class RaySmallest
 {
 	public static int go(int[] ray)
 	{
-		return 0;
+        int smallest = ray[0];
+        int i = 0;
+
+        while (i<ray.length)
+        {
+            if (ray[i]<smallest)
+            {
+                smallest = ray[i];
+            }
+            i++;
+        }
+
+		return smallest;
 	}
 }
